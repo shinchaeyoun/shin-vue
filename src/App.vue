b946eb7d3e754f40616c184ccf728c09a3fbc0e2
shinchaeyoun <jun29182877@gamil.com>
2023-05-24 16:10:40 +0900

    header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="wrap">
     <HeaderView/>
-    <MainView/>
+    <MainSectionView/>
     <FooterView/>
   </div>
 </template>
 
 <script>
 import HeaderView from './views/HeaderView.vue'
-import MainView from './views/MainView.vue'
+import MainSectionView from './views/MainSectionView.vue'
 import FooterView from './views/FooterView.vue'
 
 export default {
   components: {
     HeaderView,
-    MainView,
+    MainSectionView,
     FooterView
   }
 }
