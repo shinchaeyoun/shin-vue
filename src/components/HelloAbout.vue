9466e46e4ecc23af9e06c3a7641044cbeba27dec
shinchaeyoun <jun29182877@gamil.com>
2023-05-24 16:26:34 +0900

    header

--- src/components/HelloAbout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    hello about
-  </div>
-</template>
-
-<script>
-export default {}
-</script>
--- src/router/index.js
+++ src/router/index.js
@@ -36,16 +36,16 @@ const routes = [
     path: '/contact',
     name: 'contact',
     component: ContactView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
